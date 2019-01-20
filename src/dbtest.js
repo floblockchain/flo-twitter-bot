ec766d678d8836baf0f94923590aa75ea6b96e17
metacoin <support@colorcoin.org>
2019-01-20 00:24:24 -0500

    fixed donation tweet logic

--- src/dbtest.js
+++ src/dbtest.js
@@ -1,5 +1,6 @@
 var JsonDB = require('node-json-db');
 var db = new JsonDB("flodatabotdb", true, false);
 
-db.delete("/blockheight")
-db.delete("/block100")
+//db.delete("/blockheight")
+//db.push("/blockheight", 3219322)
+//db.delete("/block100")
